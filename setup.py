b9f6022faf8fd800b74b7dcddf399c11e57b1a33
Ben Mack <ben8mack@gmail.com>
2019-02-28 18:31:55 +0100

    status after running cookiecutter-data-science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Classifying the harmonized Landsat and Sentinel-2 data set.',
+    author='benmack',
+    license='MIT',
+)
